fe49cac1e13ffd7debe3611256169402ef6aeecd
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-04-15 01:58:23 +0900

    add simple header

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,7 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import { AuthProvider } from '@/contexts/auth';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { AuthProvider } from "@/contexts/auth";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
