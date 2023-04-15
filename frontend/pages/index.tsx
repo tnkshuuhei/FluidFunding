5f3718aa72e524ffb164a7d82166d31120e59138
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-04-15 19:18:27 +0900

    add landingpage, milestones

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,116 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import ConnectButton from "@/components/ConnectButton";
 import Header from "@/components/Header";
+import {
+  Container,
+  SimpleGrid,
+  Image,
+  Flex,
+  Heading,
+  Text,
+  Stack,
+  StackDivider,
+  Icon,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import {
+  IoAnalyticsSharp,
+  IoLogoBitcoin,
+  IoSearchSharp,
+} from "react-icons/io5";
+import { ReactElement } from "react";
 
+interface FeatureProps {
+  text: string;
+  iconBg: string;
+  icon?: ReactElement;
+}
 const inter = Inter({ subsets: ["latin"] });
-
+const Feature = ({ text, icon, iconBg }: FeatureProps) => {
+  return (
+    <Stack direction={"row"} align={"center"}>
+      <Flex
+        w={8}
+        h={8}
+        align={"center"}
+        justify={"center"}
+        rounded={"full"}
+        bg={iconBg}
+      >
+        {icon}
+      </Flex>
+      <Text fontWeight={600}>{text}</Text>
+    </Stack>
+  );
+};
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-    </main>
+    <Container maxW={"5xl"} py={12}>
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+        <Stack spacing={4}>
+          <Text
+            textTransform={"uppercase"}
+            color={"blue.400"}
+            fontWeight={600}
+            fontSize={"sm"}
+            bg={useColorModeValue("blue.50", "blue.900")}
+            p={2}
+            alignSelf={"flex-start"}
+            rounded={"md"}
+          >
+            Product Feature
+          </Text>
+          <Heading>Perpetual Public Funding Protocol(PPFG)</Heading>
+          <Text color={"gray.500"} fontSize={"lg"}>
+            PPFG is a decentralized funding protocol that allows users to create
+            and fund projects and receive funding continuously from the
+            community. By using Prediction Market, users can predict whether the
+            project will achieve certain KPIs or Milestones or not. Addicionally
+            the public goods projects can get grants from grant pool and
+            protocol will distribute grants by following Qudratic Funding Fomula
+            based on amounts of prediction. Only if the project achieves the KPI
+            or Milestone, the project will receive the grants.
+          </Text>
+          <Stack
+            spacing={4}
+            divider={
+              <StackDivider
+                borderColor={useColorModeValue("gray.100", "gray.700")}
+              />
+            }
+          >
+            <Feature
+              icon={
+                <Icon as={IoAnalyticsSharp} color={"yellow.500"} w={5} h={5} />
+              }
+              iconBg={useColorModeValue("yellow.100", "yellow.900")}
+              text={"Incentivize the community to fund public goods projects"}
+            />
+            <Feature
+              icon={<Icon as={IoLogoBitcoin} color={"green.500"} w={5} h={5} />}
+              iconBg={useColorModeValue("green.100", "green.900")}
+              text={"Prediction based Quadratic Funding"}
+            />
+            <Feature
+              icon={
+                <Icon as={IoSearchSharp} color={"purple.500"} w={5} h={5} />
+              }
+              iconBg={useColorModeValue("purple.100", "purple.900")}
+              text={"Optimize opportunities for potential projects"}
+            />
+          </Stack>
+        </Stack>
+        <Flex>
+          <Image
+            rounded={"md"}
+            alt={"feature image"}
+            src={
+              "https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+            }
+            objectFit={"cover"}
+          />
+        </Flex>
+      </SimpleGrid>
+    </Container>
   );
 }
--- frontend/pages/user/index.tsx
+++ frontend/pages/user/index.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import ProjectCard from "@/components/ProjectCard";
+
 const Home = () => {
   return (
-    <div className="p-14 w-max flex flex-col">
+    <div className="p-14 w-full flex flex-wrap justify-center">
+      <ProjectCard
+        title="Ethereum Foundation"
+        description="The Ethereum Foundation is a Swiss nonprofit, based in Zug, Switzerland. It was established in 2014 to support the development of Ethereum and related technologies."
+        price={450}
+      />
+      <ProjectCard
+        title="Ethereum Foundation"
+        description="The Ethereum Foundation is a Swiss nonprofit, based in Zug, Switzerland. It was established in 2014 to support the development of Ethereum and related technologies."
+        price={450}
+      />
       <ProjectCard
         title="Ethereum Foundation"
         description="The Ethereum Foundation is a Swiss nonprofit, based in Zug, Switzerland. It was established in 2014 to support the development of Ethereum and related technologies."
